6dd01d0751b267f47bc963dc4924431394087a18
lauren brett <laurencareybrett@gmail.com>
2020-04-08 12:03:46 +0100

    9 tests passing

--- models/park.js
+++ models/park.js
@@ -8,4 +8,13 @@ Park.prototype.addDinosaur = function (dinosaur) {
   this.dinosaurs.push(dinosaur);
 };
 
+Park.prototype.removeDinosaur = function (dinosaur) {
+  this.dinosaurs.pop(dinosaur);
+};
+
+Park.prototype.visitorsAttracted = function () {
+  for (const dinosaur of this.dinosaurs) {
+    return dinosaur.guestsAttractedPerDay;
+  }
+};
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -4,10 +4,14 @@ const Dinosaur = require("../models/dinosaur.js");
 
 describe("Park", function () {
   let dinosaur1;
+  let dinosaur2;
+
   let park;
 
   beforeEach(function () {
     dinosaur1 = new Dinosaur("abs", "meateater", 30);
+    dinosaur2 = new Dinosaur("rex", "carnivor", 20);
+
     park = new Park("Gorgie", 10);
   });
 
@@ -30,5 +34,20 @@ describe("Park", function () {
     park.addDinosaur(dinosaur1);
     const expected = [dinosaur1];
     assert.deepStrictEqual(park.dinosaurs, expected);
+  });
+
+  it("should be able to remove a dinosaur from the collection", function () {
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur1);
+    park.removeDinosaur(dinosaur2);
+    const expected = [dinosaur2];
+    assert.deepStrictEqual(park.dinosaurs, expected);
+  });
+
+  it("should be able to find how many guests are attracted to give dinosaur", function () {
+    park.addDinosaur(dinosaur1);
+    actual = park.visitorsAttracted();
+    const expected = 30;
+    assert.strictEqual(actual, expected);
   });
 });
