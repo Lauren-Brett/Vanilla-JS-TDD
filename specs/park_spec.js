9e0416cd2da5b373a3c87a588bd5a71ba42e2e88
lauren brett <laurencareybrett@gmail.com>
2020-04-08 10:50:09 +0100

    all tests passing in dinosaur, 1 test passing in park

---
+++ specs/park_spec.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+const Park = require("../models/park.js");
+
+describe("Park", function () {
+  let park;
+
+  beforeEach(function () {
+    park = new Park("Gorgie", 10);
+  });
+
+  it("should have a name", function () {
+    const actual = park.name;
+    assert.strictEqual(actual, "Gorgie");
+  });
+
+  it("should have a ticket price", function () {});
+});
