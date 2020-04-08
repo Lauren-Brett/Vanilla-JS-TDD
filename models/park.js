0efbe7b38762cc65c1b4c37e3bfbce7c6d27fba0
lauren brett <laurencareybrett@gmail.com>
2020-04-08 10:59:14 +0100

    6 tests passing

--- models/park.js
+++ models/park.js
@@ -4,4 +4,8 @@ const Park = function (name, ticketPrice) {
   this.dinosaurs = [];
 };
 
+// Park.prototype.addDinosaur = function (dinosaur) {
+//   this.dinosaurs.push(dinosaur);
+// };
+
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -1,10 +1,13 @@
 const assert = require("assert");
 const Park = require("../models/park.js");
+const Dinosaur = require("../models/dinosaur.js");
 
 describe("Park", function () {
+  let dinosaur1;
   let park;
 
   beforeEach(function () {
+    dinosaur1 = new Dinosaur("abs", "meateater", 30);
     park = new Park("Gorgie", 10);
   });
 
@@ -16,5 +19,10 @@ describe("Park", function () {
   it("should have a ticket price", function () {
     const actual = park.ticketPrice;
     assert.strictEqual(actual, 10);
+  });
+
+  it("should have a collection of dinosaurs", function () {
+    const expected = [];
+    assert.deepStrictEqual(park.dinosaurs, expected);
   });
 });
